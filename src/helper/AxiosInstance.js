9ca4ab0c80d21b310a86a937666068bd7691bd3e
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-01-16 16:18:17 +0530

    change import

--- src/helper/AxiosInstance.js
+++ src/helper/AxiosInstance.js
@@ -1,6 +1,7 @@
-const { default: axios } = require("axios");
+import axios from "axios";
 const { BASEURL } = require("../config");
 
 const AxiosInstance = axios.create({ baseURL: BASEURL, withCredentials: true })
+
 
 export default AxiosInstance;
